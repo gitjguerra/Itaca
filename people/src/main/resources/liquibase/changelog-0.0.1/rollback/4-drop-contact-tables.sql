8fa7cd906f51113d022ff33c4dd50665a56e19d6
Brian Boothe <bboothe@csi-ti.com>
2018-03-20 11:28:11 +0100

    Re-inserted original drop object commands

--- people/src/main/resources/liquibase/changelog-0.0.1/rollback/4-drop-contact-tables.sql
+++ people/src/main/resources/liquibase/changelog-0.0.1/rollback/4-drop-contact-tables.sql
@@ -9,4 +9,15 @@ DROP INDEX PER_CONTACT_ID3;
 DROP TABLE PER_CONTACT CASCADE CONSTRAINTS;
 
 -- DROP CONTACT END ****************************************************************
+
+DROP SEQUENCE per_identification_seq;
+DROP TABLE per_identification;
+
+DROP SEQUENCE per_person_detail_seq;
+DROP TABLE per_person_detail;
+
+DROP SEQUENCE per_person_seq;
+DROP TABLE per_person;
+
+
 COMMIT;
