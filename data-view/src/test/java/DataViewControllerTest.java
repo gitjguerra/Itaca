c790d5fb8f7202ac880a6355677ad7669252a2ed
jguerra <cervecera.artesanal@gmail.com>
2018-06-04 08:58:16 -0430

    Test Odata

--- data-view/src/test/java/DataViewControllerTest.java
+++ data-view/src/test/java/DataViewControllerTest.java
@@ -1,12 +1,6 @@
-import com.csi.itaca.common.model.dto.CountryDTO;
 import com.csi.itaca.dataview.controller.DataViewController;
 import com.csi.itaca.dataview.service.EntityProvider;
-import com.csi.itaca.tools.utils.jpa.Pagination;
-import com.csi.itaca.tools.utils.json.JsonUtils;
 import org.apache.olingo.commons.api.data.EntityCollection;
-import org.apache.olingo.server.api.uri.UriInfo;
-import org.apache.olingo.server.api.uri.queryoption.expression.Expression;
-import org.apache.olingo.server.core.uri.UriInfoImpl;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -15,34 +9,15 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.springframework.http.MediaType;
 import org.springframework.restdocs.JUnitRestDocumentation;
-import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.validation.Errors;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-
-import static org.hamcrest.Matchers.is;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.mockito.Matchers.any;
-
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-
 
 /**
  * Test for the people management rest controller.
