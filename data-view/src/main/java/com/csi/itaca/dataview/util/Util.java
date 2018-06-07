97ef19d5be58a1cb1e042e556ec4a855eb4f4655
jose.guerra@itaca.tech <cervecera.artesanal@gmail.com>
2018-06-07 10:12:03 -0430

    implement $count

--- data-view/src/main/java/com/csi/itaca/dataview/util/Util.java
+++ data-view/src/main/java/com/csi/itaca/dataview/util/Util.java
@@ -1,11 +1,16 @@
-package com.csi.itaca.dataview.edm;
+package com.csi.itaca.dataview.util;
 
 import java.util.List;
 import java.util.Locale;
 
 import org.apache.olingo.commons.api.data.Entity;
 import org.apache.olingo.commons.api.data.EntityCollection;
-import org.apache.olingo.commons.api.edm.*;
+import org.apache.olingo.commons.api.edm.EdmEntitySet;
+import org.apache.olingo.commons.api.edm.EdmEntityType;
+import org.apache.olingo.commons.api.edm.EdmPrimitiveType;
+import org.apache.olingo.commons.api.edm.EdmPrimitiveTypeException;
+import org.apache.olingo.commons.api.edm.EdmProperty;
+import org.apache.olingo.commons.api.edm.EdmType;
 import org.apache.olingo.commons.api.http.HttpStatusCode;
 import org.apache.olingo.server.api.ODataApplicationException;
 import org.apache.olingo.server.api.uri.UriInfoResource;
@@ -16,12 +21,11 @@ import org.apache.olingo.server.api.uri.UriResourceEntitySet;
 public class Util {
 
     public static EdmEntitySet getEdmEntitySet(UriInfoResource uriInfo) throws ODataApplicationException {
-
         List<UriResource> resourcePaths = uriInfo.getUriResourceParts();
         // To get the entity set we have to interpret all URI segments
         if (!(resourcePaths.get(0) instanceof UriResourceEntitySet)) {
-            throw new ODataApplicationException("Invalid resource type for first segment.",
-                    HttpStatusCode.NOT_IMPLEMENTED.getStatusCode(),Locale.ENGLISH);
+            // Here we should interpret the whole URI but in this example we do not support navigation so we throw an exception
+            throw new ODataApplicationException("Invalid resource type for first segment.", HttpStatusCode.NOT_IMPLEMENTED.getStatusCode(),Locale.ENGLISH);
         }
 
         UriResourceEntitySet uriResource = (UriResourceEntitySet) resourcePaths.get(0);
@@ -29,18 +33,16 @@ public class Util {
         return uriResource.getEntitySet();
     }
 
-    public static Entity findEntity(EdmEntityType edmEntityType,
-                                    EntityCollection rt_entitySet, List<UriParameter> keyParams)
-            throws ODataApplicationException {
 
-        List<Entity> entityList = rt_entitySet.getEntities();
+    public static Entity findEntity(EdmEntityType edmEntityType, EntityCollection entitySet, List<UriParameter> keyParams) throws ODataApplicationException {
 
-        // loop over all entities in order to find that one that matches all keys in request
-        // an example could be e.g. contacts(ContactID=1, CompanyID=1)
-        for(Entity rt_entity : entityList){
-            boolean foundEntity = entityMatchesAllKeys(edmEntityType, rt_entity, keyParams);
-            if(foundEntity){
-                return rt_entity;
+        List<Entity> entityList = entitySet.getEntities();
+
+        // loop over all entities in order to find that one that matches all keys in request e.g. contacts(ContactID=1, CompanyID=1)
+        for(Entity entity : entityList){
+            boolean foundEntity = entityMatchesAllKeys(edmEntityType, entity, keyParams);
+            if(foundEntity) {
+                return entity;
             }
         }
 
@@ -48,7 +50,8 @@ public class Util {
     }
 
 
-    public static boolean entityMatchesAllKeys(EdmEntityType edmEntityType, Entity rt_entity,  List<UriParameter> keyParams)
+    public static boolean
+    entityMatchesAllKeys(EdmEntityType edmEntityType, Entity rt_entity, List<UriParameter> keyParams)
             throws ODataApplicationException {
 
         // loop over all keys
@@ -58,7 +61,7 @@ public class Util {
             String keyText = key.getText();
 
             // Edm: we need this info for the comparison below
-            EdmProperty edmKeyProperty = (EdmProperty )edmEntityType.getProperty(keyName);
+            EdmProperty edmKeyProperty = (EdmProperty) edmEntityType.getProperty(keyName);
             Boolean isNullable = edmKeyProperty.isNullable();
             Integer maxLength = edmKeyProperty.getMaxLength();
             Integer precision = edmKeyProperty.getPrecision();
@@ -66,29 +69,29 @@ public class Util {
             Integer scale = edmKeyProperty.getScale();
             // get the EdmType in order to compare
             EdmType edmType = edmKeyProperty.getType();
-            // Key properties must be instance of primitive type
-            EdmPrimitiveType edmPrimitiveType = (EdmPrimitiveType)edmType;
+            // if(EdmType instanceof EdmPrimitiveType) // do we need this?
+            EdmPrimitiveType edmPrimitiveType = (EdmPrimitiveType) edmType;
 
             // Runtime data: the value of the current entity
-            Object valueObject = rt_entity.getProperty(keyName).getValue(); // null-check is done in FWK
+            // don't need to check for null, this is done in olingo library
+            Object valueObject = rt_entity.getProperty(keyName).getValue();
 
             // now need to compare the valueObject with the keyText String
-            // this is done using the type.valueToString //
+            // this is done using type.valueToString
             String valueAsString = null;
             try {
-                valueAsString = edmPrimitiveType.valueToString(valueObject, isNullable, maxLength,
-                        precision, scale, isUnicode);
+                valueAsString = edmPrimitiveType.valueToString(valueObject, isNullable, maxLength, precision, scale, isUnicode);
             } catch (EdmPrimitiveTypeException e) {
                 throw new ODataApplicationException("Failed to retrieve String value",
-                        HttpStatusCode.INTERNAL_SERVER_ERROR.getStatusCode(),Locale.ENGLISH, e);
+                        HttpStatusCode.INTERNAL_SERVER_ERROR.getStatusCode(), Locale.ENGLISH, e);
             }
 
-            if (valueAsString == null){
+            if (valueAsString == null) {
                 return false;
             }
 
             boolean matches = valueAsString.equals(keyText);
-            if(!matches){
+            if (!matches) {
                 // if any of the key properties is not found in the entity, we don't need to search further
                 return false;
             }
