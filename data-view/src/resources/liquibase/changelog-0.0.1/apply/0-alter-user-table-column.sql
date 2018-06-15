19dfa695254ca35f46635251d90084d94d4c299a
jose.guerra@itaca.tech <cervecera.artesanal@gmail.com>
2018-06-14 19:30:49 -0430

    se agregó script para alter table de USR_USER

---
+++ data-view/src/resources/liquibase/changelog-0.0.1/apply/0-alter-user-table-column.sql
@@ -0,0 +1,5 @@
+----------------- Modified the user table -----------------
+-- The column password are the varchar2(50) and is neccesary change to varchar2(100)
+ALTER TABLE ITACA.USR_USER MODIFY PASSWORD varchar2(100);
+----------------- Modified the user table  -----------------
+
