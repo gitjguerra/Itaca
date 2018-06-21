1f32c9f6948070d7f95c88ac408330b1261ac854
jose.guerra@itaca.tech <cervecera.artesanal@gmail.com>
2018-06-20 19:48:16 -0430

    Cors

--- user/src/main/java/com/csi/itaca/users/config/CorsFilter.java
+++ user/src/main/java/com/csi/itaca/users/config/CorsFilter.java
@@ -14,6 +14,14 @@ public class CorsFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authCredentials = request.getHeader("Authorization");
+
+        if(authCredentials == null) {
+            //logger.info("Request with no basic auth credentials {}", request.getRequestURL());
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
