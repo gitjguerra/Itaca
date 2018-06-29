8834e340c336d4f75771e44c615fab17681da115
jose.guerra@itaca.tech <cervecera.artesanal@gmail.com>
2018-06-29 00:29:33 -0430

    Ajuste modulo Load

--- load/src/main/java/com/csi/itaca/load/endpoint/LoadManagementRestController.java
+++ load/src/main/java/com/csi/itaca/load/endpoint/LoadManagementRestController.java
@@ -3,31 +3,16 @@ package com.csi.itaca.load.endpoint;
 import com.csi.itaca.common.endpoint.ItacaBaseRestController;
 import com.csi.itaca.load.api.LoadManagementServiceProxy;
 import com.csi.itaca.load.service.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @SuppressWarnings("unchecked")
 @RestController
@@ -49,9 +34,6 @@ public class LoadManagementRestController extends ItacaBaseRestController implem
 
     @Autowired
     private LoadManagementService loadManagementService;
-
-    @Autowired
-    private JobCompletionNotificationListener jobCompletionNotificationListener;
 
     @Override
     public ResponseEntity<List<String>> Create(Model model) {
