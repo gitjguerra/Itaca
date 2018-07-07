4ab4c0486e92b22a865d7aeee0c151edd0c960e8
jose.guerra@itaca.tech <cervecera.artesanal@gmail.com>
2018-07-07 00:58:03 -0430

    prueba con campos de longitud fija - ajuste

--- web/src/main/java/com/csi/itaca/ItacaApplication.java
+++ web/src/main/java/com/csi/itaca/ItacaApplication.java
@@ -2,18 +2,13 @@ package com.csi.itaca;
 
 import com.csi.itaca.config.model.Configurator;
 import com.csi.itaca.config.tools.ConfiguratorImpl;
-import config.modules.CORSFilter;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import javax.annotation.PostConstruct;
-import javax.servlet.Filter;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @ServletComponentScan
